31f62938583845f56201c82edcc077bcc9ab02e9
ThanhSon02 <thanhson.hw@gmail.com>
2022-12-11 22:20:46 +0700

    Hoàn thành logic phần scroll của Hero providers

--- src/components/HeroProvider/HeroProvider.js
+++ src/components/HeroProvider/HeroProvider.js
@@ -3,10 +3,14 @@ import styles from './HeroProvider.module.scss';
 import images from '~/assets/img';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
+import { useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function HeroProvider({ small = false }) {
+    const [scrollPos, setScrollPos] = useState(0);
+    const providerList = useRef();
+
     const providers = [
         {
             alt: 'amazoneprime',
@@ -147,10 +151,154 @@ function HeroProvider({ small = false }) {
             alt: 'youtobe',
             image: images.providers.youtobe,
         },
+
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'yupptv',
+            image: images.providers.yupptv,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
+        {
+            alt: 'youtobe',
+            image: images.providers.youtobe,
+        },
     ];
+
+    const handlePrev = () => {
+        let scrollPos = providerList.current.scrollLeft;
+        providerList.current.scroll({
+            left: scrollPos - 740,
+            behavior: 'smooth',
+        });
+    };
+
+    const handleNext = () => {
+        let scrollPos = providerList.current.scrollLeft;
+        providerList.current.scroll({
+            left: scrollPos + 740,
+            behavior: 'smooth',
+        });
+    };
+
+    let btnNext;
+    if (scrollPos !== 0) {
+        btnNext = (
+            <div className={cx('btn-prev')}>
+                <FontAwesomeIcon icon={faAngleLeft} onClick={handlePrev} />
+            </div>
+        );
+    } else {
+        btnNext = <></>;
+    }
+
     return (
         <div className={cx('wrapper')}>
-            <ul className={cx('provider-list')}>
+            <ul
+                ref={providerList}
+                className={cx('provider-list')}
+                onScroll={(e) => {
+                    setScrollPos(e.target.scrollLeft);
+                }}
+            >
                 {providers.map((provider, index) => (
                     <li key={index} className={cx('provider-item', { small })}>
                         <img src={provider.image} alt={provider.alt} />
@@ -162,10 +310,8 @@ function HeroProvider({ small = false }) {
             </ul>
             {providers.length > 33 ? (
                 <>
-                    <div className={cx('btn-prev')}>
-                        <FontAwesomeIcon icon={faAngleLeft} />
-                    </div>
-                    <div className={cx('btn-next')}>
+                    {btnNext}
+                    <div className={cx('btn-next')} onClick={handleNext}>
                         <FontAwesomeIcon icon={faAngleRight} />
                     </div>
                 </>
